0a59bf9fefb2a233ddcc8d6ae88983f3607cb9a1
Amal Junaid <aj9637@rit.edu>
2026-02-17 13:56:11 +0400

    added Movie Bean

---
+++ Project/src/main/java/org/example/project/model/BeanConfig.java
@@ -0,0 +1,10 @@
+package org.example.project.model;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan("org.example.project.model.data")
+public class BeanConfig {
+}
---
+++ Project/src/main/java/org/example/project/model/data/Movie.java
@@ -0,0 +1,10 @@
+package org.example.project.model.data;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Movie {
+    private String movieName;
+    private String director;
+
+}
