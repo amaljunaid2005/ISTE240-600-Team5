cba3a4e06e89819794ed13a0e83318b8754a5b1d
ms4830 <ms4830@g.rit.edu>
2026-02-19 11:29:05 +0400

    userprofile with getters and setters

---
+++ Project/src/main/java/org/example/project/model/data/UserProfile.java
@@ -0,0 +1,65 @@
+package org.example.project.model.data;
+
+import org.springframework.stereotype.Component;
+@Component
+public class UserProfile {
+    private int UserID;
+    private String username;
+    private String email;
+    private double age;
+    private String password;
+    private String bio;
+    //profile picture//
+
+
+    public int getUserID() {
+        return UserID;
+    }
+
+
+    public void setUserID(int userID) {
+        UserID = userID;
+    }
+
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getAge() {
+        return age;
+    }
+
+    public void setAge(double age) {
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
