60d06ccd7885f84f056f95703e40d85dc4212309
Amal Junaid <aj9637@rit.edu>
2026-02-19 10:46:37 +0400

    added more attributes, getters and setters

--- Project/src/main/java/org/example/project/model/data/Movie.java
+++ Project/src/main/java/org/example/project/model/data/Movie.java
@@ -4,7 +4,49 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Movie {
+    private int id;
     private String movieName;
     private String director;
+    private double rating;
+    private String leadActor;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getLeadActor() {
+        return leadActor;
+    }
+
+    public void setLeadActor(String leadActor) {
+        this.leadActor = leadActor;
+    }
 }
