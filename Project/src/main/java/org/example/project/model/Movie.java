bb0334e4ff574295634e98a012fb5e26f6e1325a
Amal Junaid <aj9637@rit.edu>
2026-03-13 15:40:12 +0400

    added movie list to service class

--- Project/src/main/java/org/example/project/model/Movie.java
+++ Project/src/main/java/org/example/project/model/Movie.java
@@ -5,69 +5,66 @@ import org.springframework.stereotype.Component;
  * Represents a Movie that can be reviewed or added to a favorites list
  * Includes a unique id, the name of the movie, its genre, director and lead actor as well as a short description of its premise.
  */
-@Component
 public class Movie {
     private int id;
     private String movieName;
+    private int releaseYear
     private String director;
     private double rating;
     private String leadActor;
     private String genre;
     private String description;
 
+    public Movie(int id, String movieName,int releaseYear, String director, double rating, String leadActor, String genre, String description) {
+        this.id = id;
+        this.movieName = movieName;
+        this.releaseYear=releaseYear;
+        this.director = director;
+        this.rating = rating;
+        this.leadActor = leadActor;
+        this.genre = genre;
+        this.description = description;
+    }
+
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+
 
     public String getGenre() {
         return genre;
     }
 
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
+
 
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+
 
     public String getMovieName() {
         return movieName;
     }
 
-    public void setMovieName(String movieName) {
-        this.movieName = movieName;
-    }
+
 
     public String getDirector() {
         return director;
     }
 
-    public void setDirector(String director) {
-        this.director = director;
-    }
+
 
     public double getRating() {
         return rating;
     }
 
-    public void setRating(double rating) {
-        this.rating = rating;
-    }
+
 
     public String getLeadActor() {
         return leadActor;
     }
 
-    public void setLeadActor(String leadActor) {
-        this.leadActor = leadActor;
-    }
+
 }
--- Project/src/main/java/org/example/project/services/MainService.java
+++ Project/src/main/java/org/example/project/services/MainService.java
@@ -1,7 +1,20 @@
 package org.example.project.services;
 
+import org.example.project.model.Movie;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class MainService {
+    List<Movie> movieList = new ArrayList<Movie>();
+    public MainService(){
+        var movie1= new Movie(1,"Matilda",1996,"Danny DeVito",7,"Mara Wilson","Family"," girl gifted with a keen intellect and psychic powers uses both to get even with her callous family and free her kindly schoolteacher from the tyrannical grip of a sadistic headmistress.");
+        var movie2= new Movie(2,"Midsommar",2019,"Ari Aster",7.1,"Florence Pugh","horror","A couple travels to Northern Europe to visit a rural hometown's fabled Swedish mid-summer festival. What begins as an idyllic retreat quickly devolves into an increasingly violent and bizarre competition at the hands of a pagan cult.");
+        this.movieList.add(movie1);
+        this.movieList.add(movie2);
+    }
+    public List<Movie> findAllMovies(){return this.movieList;}
+    public void saveMovie(Movie movie){this.movieList.add(movie);}
 }
