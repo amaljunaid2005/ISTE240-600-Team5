c1dc84576b4b30363199876cc7fe61f3b965959e
Amal Junaid <aj9637@rit.edu>
2026-04-23 17:47:22 +0400

    added deletebyid and changemoviename methods, changed datatype of id back to int

--- Project/src/main/java/org/example/project/model/Movie.java
+++ Project/src/main/java/org/example/project/model/Movie.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class Movie {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private int id;
 
     @Column(name="movie_name", length = 100,nullable = false)
     private String movieName;
@@ -52,7 +52,7 @@ public class Movie {
         this.description = description;
     }
 
-    public long getId() {
+    public int getId() {
         return id;
     }
 
--- Project/src/main/java/org/example/project/repositories/MovieRepository.java
+++ Project/src/main/java/org/example/project/repositories/MovieRepository.java
@@ -1,19 +1,28 @@
 package org.example.project.repositories;
 
 import org.example.project.model.Movie;
+import org.hibernate.sql.Update;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface MovieRepository extends JpaRepository<Movie,Long> {
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
     //Find methods, findAll already exists by default
-    Optional<Movie> findById(long id);
+    Optional<Movie> findById(int id);
 
     @Query("select m from Movie m where m.movieName=:name")
-    List<Movie> findByMovieName(String name);
+    List<Movie> findByMovieName(@Param("name") String name);
 
+    //Update methods
+    @Modifying
+    @Query("update Movie m set m.movieName=:name where m.id=:id")
+    int changeMovieName(@Param("id") int id, @Param("name") String name);
 
+    //Delete methods
+    void deleteById(long id);
 
 }
