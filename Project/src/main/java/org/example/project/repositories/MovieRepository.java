047411dd37ddbf7f0f97ee4aeb964a62a3be0720
Amal Junaid <aj9637@rit.edu>
2026-04-23 17:21:52 +0400

    added find methods in movie repository

--- Project/src/main/java/org/example/project/repositories/MovieRepository.java
+++ Project/src/main/java/org/example/project/repositories/MovieRepository.java
@@ -2,7 +2,18 @@ package org.example.project.repositories;
 
 import org.example.project.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface MovieRepository extends JpaRepository<Movie,Long> {
+    //Find methods, findAll already exists by default
+    Optional<Movie> findById(long id);
+
+    @Query("select m from Movie m where m.movieName=:name")
+    List<Movie> findByMovieName(String name);
+
+
 
 }
