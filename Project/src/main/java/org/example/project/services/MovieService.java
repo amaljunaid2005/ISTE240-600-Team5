2aaf4e88042471d3c9d72f4976115a38f105a0c7
Amal Junaid <aj9637@rit.edu>
2026-04-23 18:15:46 +0400

    added saveMovie, getMovieById and getAllMovies methods to MovieService

--- Project/src/main/java/org/example/project/services/MovieService.java
+++ Project/src/main/java/org/example/project/services/MovieService.java
@@ -1,13 +1,30 @@
 package org.example.project.services;
 
 import jakarta.transaction.Transactional;
+import org.example.project.model.Movie;
 import org.example.project.repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
 public class MovieService {
     @Autowired
     MovieRepository movieRepo;
+
+    public Movie saveMovie(Movie movieToSave){
+        return movieRepo.save(movieToSave);
+    }
+
+    public List<Movie> getAllMovies(){
+        return movieRepo.findAll();
+    }
+
+    public Optional<Movie> getMovieById(int id){
+        return movieRepo.findById(id);
+    }
+
 }
