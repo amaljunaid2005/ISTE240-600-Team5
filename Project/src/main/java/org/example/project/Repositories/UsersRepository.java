b61e76613bd27363d7ce861d453556c259674ea4
madeeha <madheeha.sameen@gmail.com>
2026-04-25 19:39:51 +0400

    extended jparepository

--- Project/src/main/java/org/example/project/Repositories/UsersRepository.java
+++ Project/src/main/java/org/example/project/Repositories/UsersRepository.java
@@ -1,4 +1,9 @@
 package org.example.project.Repositories;
 
-public class UsersRepository {
+import org.example.project.model.UserProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepository extends JpaRepository<UserProfile,Integer> {
+
+
 }
--- Project/src/main/java/org/example/project/services/UserService.java
+++ Project/src/main/java/org/example/project/services/UserService.java
@@ -1,4 +1,6 @@
 package org.example.project.services;
 
 public class UserService {
+
+
 }
