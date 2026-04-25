9a58e5a472a2531e9c76bb75745a0a640087f450
madeeha <madheeha.sameen@gmail.com>
2026-04-25 21:09:11 +0400

    added findByid method and findBy field method

--- Project/src/main/java/org/example/project/Repositories/UsersRepository.java
+++ Project/src/main/java/org/example/project/Repositories/UsersRepository.java
@@ -3,7 +3,18 @@ package org.example.project.Repositories;
 import org.example.project.model.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UsersRepository extends JpaRepository<UserProfile,Integer> {
 
+//Find methods
+    //findAll already exists by default
+
+    //find byid
+    Optional<UserProfile> finById(int id);
+
+    //find by field
+    List<UserProfile> findByUsername(String username);
 
 }
