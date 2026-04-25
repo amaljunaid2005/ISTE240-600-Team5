2ed05865ac7425965419cac4f74ab7270d9e6d04
madeeha <madheeha.sameen@gmail.com>
2026-04-25 21:57:35 +0400

    connected userservice and userepository

--- Project/src/main/java/org/example/project/services/UserService.java
+++ Project/src/main/java/org/example/project/services/UserService.java
@@ -1,6 +1,19 @@
 package org.example.project.services;
 
+
+import jakarta.transaction.Transactional;
+import org.example.project.Repositories.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
 public class UserService {
+    @Autowired
+    UsersRepository usersRepository;
+
+    
+
 
 
 }
