0548015e173fd3430d30242861f5560dcb0b69bc
madeeha <madheeha.sameen@gmail.com>
2026-04-25 21:46:52 +0400

    added update method , deleteby method , if exists method

--- Project/src/main/java/org/example/project/Repositories/UsersRepository.java
+++ Project/src/main/java/org/example/project/Repositories/UsersRepository.java
@@ -1,7 +1,13 @@
 package org.example.project.Repositories;
 
+import jakarta.transaction.Transactional;
 import org.example.project.model.UserProfile;
+import org.hibernate.sql.Update;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 
 import java.util.List;
 import java.util.Optional;
@@ -14,7 +20,23 @@ public interface UsersRepository extends JpaRepository<UserProfile,Integer> {
     //find byid
     Optional<UserProfile> finById(int id);
 
+
+    //update method
+    @Modifying
+    @Query("update UserProfile u set u.email = :email WHERE u.userId = :id")
+    int updateEmailById(@Param("id") Integer id, @Param("email") String email);
+
+
     //find by field
     List<UserProfile> findByUsername(String username);
+
+    //delete method
+
+    void deleteById(int id);
+
+    //to see if record exists
+    boolean existsByEmail(String email);
+
+
 
 }
