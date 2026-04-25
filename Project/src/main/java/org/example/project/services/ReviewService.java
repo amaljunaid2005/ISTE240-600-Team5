1f2aeda302cb2b509390c917d55498b91796e0b3
mf7807 <mf7807@rit.edu>
2026-04-25 15:05:58 +0400

    Add @Transactional annotation to ReviewService and saveReview method for saving new reviews

--- Project/src/main/java/org/example/project/services/ReviewService.java
+++ Project/src/main/java/org/example/project/services/ReviewService.java
@@ -1,8 +1,30 @@
 package org.example.project.services;
 
+import jakarta.transaction.Transactional;
+import org.example.project.dataLayer.ReviewRepository;
+import org.example.project.models.Review;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
 public class ReviewService {
+        ReviewRepository reviewRepository;
+
+        public Review saveReview(Review reviewToSave){
+
+            Review existingReview = reviewRepository.findByMovieAndUserProfile(reviewToSave.getMovie(), reviewToSave.getUserProfile()).orElse(null);
+            if (existingReview != null) {
+                throw new IllegalArgumentException("You have already reviewed this movie.");
+            }
+
+            Review newReview = new Review();
+            newReview.setReviewText(reviewToSave.getReviewText());
+            newReview.setUserProfile(reviewToSave.getUserProfile());
+            newReview.setMovie(reviewToSave.getMovie());
+            newReview.setRating(reviewToSave.getRating());
+            newReview.setReviewDate(reviewToSave.getReviewDate());
+
+            return reviewRepository.save(newReview);
+        }
+
 }
