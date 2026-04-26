151e9c5c87e7ef369baeaaee1c16f5ef8fbceb80
gr15hma <gb2109@g.rit.edu>
2026-04-26 19:21:18 +0545

    Update and rename TicketRepository to TicketRepository.java
    changed repository to repositories

--- Project/src/main/java/org/example/project/repositories/TicketRepository.java
+++ Project/src/main/java/org/example/project/repositories/TicketRepository.java
@@ -1,9 +1,8 @@
 //GRISHMA BHANDARI 761001853
-package com.moviereview.repository;
+package org.example.project.repositories;
 
-import org.example.project.models.Ticket
-import org.example.project.models.Movie;
-import org.example.project.models.User;;
+import org.example.project.models.Ticket;
+import org.example.project.models.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,6 +13,6 @@ public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
     List<Ticket> findByUser(UserProfile user);
 
-    @Query("SELECT t FROM Ticket t WHERE LOWER(t.movie.title) LIKE LOWER(CONCAT('%', :title, '%'))")
+    @Query("SELECT t FROM Ticket t WHERE LOWER(t.movie.movieName) LIKE LOWER(CONCAT('%', :title, '%'))")
     List<Ticket> findByMovieTitleContaining(@Param("title") String title);
 }
